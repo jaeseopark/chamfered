06caa75b9b58c997c33d0daa6e50ff25df78ccd1
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2026-09-23 21:24:06 -0600

    feat: add grey

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,9 @@ export const APPS = [
     description: 'Visualize and compare how variable-aperture zoom lenses change aperture across focal lengths',
     url: 'https://aperture.chamfered.dev',
   },
+  {
+    name: 'Grey',
+    description: 'Fully local image editor for scanned greyscale images like receipts',
+    url: 'https://grey.chamfered.dev',
+  },
 ] as const;
